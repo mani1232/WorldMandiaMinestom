61bfdc7f6a73702dec2fd4bb3249275c02198514
mani1232 <support@worldmandia.cc>
2024-07-19 15:13:13 +0200

    Fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,6 @@ dependencyResolutionManagement {
         maven("https://repo.worldmandia.cc/releases")
         maven("https://jitpack.io")
         mavenCentral()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
