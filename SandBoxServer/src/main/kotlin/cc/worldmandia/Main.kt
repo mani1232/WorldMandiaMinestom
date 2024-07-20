9b64dab435be702d2733865890d2c1999ef32c6f
mani1232 <support@worldmandia.cc>
2024-07-20 22:37:36 +0200

    Resize main method

--- SandBoxServer/src/main/kotlin/cc/worldmandia/Main.kt
+++ SandBoxServer/src/main/kotlin/cc/worldmandia/Main.kt
@@ -2,28 +2,21 @@ package cc.worldmandia
 
 import cc.worldmandia.configuration.files.ConfigFiles
 import io.klogging.logger
-import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
 val LOGGER = logger("General")
 
-fun main(): Unit = runBlocking {
-    val time = measureExecutionTime {
-        ConfigFiles.init()
-        val server = WMMinecraftServer(
-            address = ConfigFiles.config.data.address,
-            port = ConfigFiles.config.data.port
-        )
-        server.start()
-        server.enableSkinsListener()
-        server.enableAutoSave()
-    }
-
-    LOGGER.info { "Done! (${time}ms)" }
-}
-
-suspend fun measureExecutionTime(block: suspend () -> Unit): Long {
-    return measureTimeMillis {
-        block()
-    }
+suspend fun main(): Unit = LOGGER.info {
+    "Done! (${
+        measureTimeMillis {
+            ConfigFiles.init()
+            val server = WMMinecraftServer(
+                address = ConfigFiles.config.data.address,
+                port = ConfigFiles.config.data.port
+            )
+            server.start()
+            server.enableSkinsListener()
+            server.enableAutoSave()
+        }
+    }ms)"
 }
