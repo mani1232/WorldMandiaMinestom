df47d91e51e72b91c495ca84bf4b8d615b1f347b
mani1232 <support@worldmandia.cc>
2024-07-20 23:00:10 +0200

    OK

--- SandBoxServer/src/main/kotlin/cc/worldmandia/Main.kt
+++ SandBoxServer/src/main/kotlin/cc/worldmandia/Main.kt
@@ -2,6 +2,8 @@ package cc.worldmandia
 
 import cc.worldmandia.configuration.files.ConfigFiles
 import io.klogging.logger
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
 val LOGGER = logger("General")
@@ -9,14 +11,18 @@ val LOGGER = logger("General")
 suspend fun main(): Unit = LOGGER.info {
     "Done! (${
         measureTimeMillis {
-            ConfigFiles.init()
-            val server = WMMinecraftServer(
-                address = ConfigFiles.config.data.address,
-                port = ConfigFiles.config.data.port
-            )
-            server.start()
-            server.enableSkinsListener()
-            server.enableAutoSave()
+            runBlocking { 
+                launch {
+                    ConfigFiles.init()
+                    val server = WMMinecraftServer(
+                        address = ConfigFiles.config.data.address,
+                        port = ConfigFiles.config.data.port
+                    )
+                    server.start()
+                    server.enableSkinsListener()
+                    server.enableAutoSave()
+                }
+            }
         }
     }ms)"
 }
